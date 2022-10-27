db86c47e9b642e1695154e1af5fcab11cc6bbc28
chenym1992 <18251821296@163.com>
2022-10-27 16:32:52 +0800

    chore: buy me a coffee frontend

--- 2. How to Build "Buy Me a Coffee" DeFi dapp/scripts/withdraw.js
+++ 2. How to Build "Buy Me a Coffee" DeFi dapp/scripts/withdraw.js
@@ -5,38 +5,51 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const {abi} = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
+const { abi } = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
 const { BNB_TEST_URL, PRIVATE_KEY } = process.env;
 async function getBalance(provider, address) {
-  const balanceBigInt = await provider.getBalance(address);
-  return hre.ethers.utils.formatEther(balanceBigInt);
+    const balanceBigInt = await provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
 }
 async function main() {
-  // Get the contract that has been deployed to BSC test.
-  const contractAddress="0xc0CF2b848F5f7BB8adFdD9137DCCA82D7A0Bd3c6";
-  const contractABI = abi;
+    // Get the contract that has been deployed to BSC test.
+    const contractAddress = "0xc0CF2b848F5f7BB8adFdD9137DCCA82D7A0Bd3c6";
+    const contractABI = abi;
 
-  // Get the node connection and wallet connection.
-  const provider = new hre.ethers.providers.JsonRpcProvider(BNB_TEST_URL);
+    // Get the node connection and wallet connection.
+    const provider = new hre.ethers.providers.JsonRpcProvider(BNB_TEST_URL);
 
-  // Ensure that signer is the SAME address as the original contract deployer,
-  // or else this script will fail with an error.
-  const signer = new hre.ethers.Wallet(PRIVATE_KEY, provider);
-  
-  // Instantiate connected contract.
-  const buyMeACoffee = new hre.ethers.Contract(contractAddress, contractABI, signer);
+    // Ensure that signer is the SAME address as the original contract deployer,
+    // or else this script will fail with an error.
+    const signer = new hre.ethers.Wallet(PRIVATE_KEY, provider);
+
+    // Instantiate connected contract.
+    const buyMeACoffee = new hre.ethers.Contract(contractAddress, contractABI, signer);
+
+    console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
+    // Check starting balances.
+    console.log("current balance of owner: ", await getBalance(provider, signer.address), "tBNB");
+    const contractBalance = await getBalance(provider, buyMeACoffee.address);
+    console.log("current balance of contract: ", contractBalance, "tBNB");
+
+    // Withdraw funds if there are funds to withdraw.
+    if (contractBalance !== "0.0") {
+        console.log("withdrawing funds..")
+        const withdrawTxn = await buyMeACoffee.withdrawTips();
+        await withdrawTxn.wait();
+    } else {
+        console.log("no funds to withdraw!");
+    }
+
+    // Check ending balance.
+    console.log("current balance of owner: ", await getBalance(provider, signer.address), "tBNB");
     
-  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
-  // Check starting balances.
-  console.log("current balance of owner: ", await getBalance(provider, signer.address), "tBNB");
-  const contractBalance = await getBalance(provider, buyMeACoffee.address);
-  console.log("current balance of contract: ", contractBalance, "tBNB");
 
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
---
+++ buy-me-a-coffee-frontend/src/App.jsx
@@ -0,0 +1,251 @@
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+import { abi } from '../../2. How to Build "Buy Me a Coffee" DeFi dapp/artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json';
+import { ethers } from "ethers";
+function App() {
+  // Contract Address & ABI
+  const contractAddress = "0xc0CF2b848F5f7BB8adFdD9137DCCA82D7A0Bd3c6";
+  const contractABI = abi;
+
+  // Component state
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [memos, setMemos] = useState([]);
+
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const onMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      console.log("accounts: ", accounts);
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+        setCurrentAccount(account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("please install MetaMask");
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const buyCoffee = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+        const buyMeACoffee = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        console.log("buying coffee..");
+        const coffeeTxn = await buyMeACoffee.buyCoffee(
+          name ? name : "anon",
+          message ? message : "Enjoy your coffee!",
+          { value: ethers.utils.parseEther("0.001") }
+        );
+
+        await coffeeTxn.wait();
+
+        console.log("mined ", coffeeTxn.hash);
+
+        console.log("coffee purchased!");
+
+        // Clear the form fields.
+        setName("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Function to fetch all memos stored on-chain.
+  const getMemos = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const buyMeACoffee = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        
+        console.log("fetching memos from the blockchain..");
+        const memos = await buyMeACoffee.getMemos();
+        console.log("fetched!", memos);
+        setMemos(memos);
+      } else {
+        console.log("Metamask is not connected");
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    let buyMeACoffee;
+    isWalletConnected();
+    getMemos();
+
+    // Create an event handler function for when someone sends
+    // us a new memo.
+    const onNewMemo = (from, timestamp, name, message) => {
+      console.log("Memo received: ", from, timestamp, name, message);
+      setMemos((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: timestamp,
+          message,
+          name
+        }
+      ]);
+    };
+
+    const {ethereum} = window;
+
+    // Listen for new memo events.
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const signer = provider.getSigner();
+      buyMeACoffee = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      buyMeACoffee.on("NewMemo", onNewMemo);
+    }
+
+    return () => {
+      if (buyMeACoffee) {
+        buyMeACoffee.off("NewMemo", onNewMemo);
+      }
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Buy Me a Coffee!
+        </h1>
+        
+        {currentAccount ? (
+          <div>
+            <form>
+              <div>
+                <label>
+                  Name
+                </label>
+                <br/>
+                
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="anon"
+                  onChange={onNameChange}
+                  />
+              </div>
+              <br/>
+              <div>
+                <label>
+                  Send Albert a message
+                </label>
+                <br/>
+
+                <textarea
+                  rows={3}
+                  placeholder="Enjoy your coffee!"
+                  id="message"
+                  onChange={onMessageChange}
+                  required
+                >
+                </textarea>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  onClick={buyCoffee}
+                >
+                  Send 1 Coffee for 0.001tBNB
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <button onClick={connectWallet}> Connect your wallet </button>
+        )}
+      </main>
+
+      {currentAccount && (<h1>Memos received</h1>)}
+
+      {currentAccount && (memos.map((memo, idx) => {
+        const dateString =  new Date(memo.timestamp.toNumber()*1000).toString()
+        let message 
+        try {
+          message = memo.message.toString()
+        } catch (error) {
+          message = error.message
+        }
+        return (
+          <div key={idx} style={{border:"2px solid", "borderRadius":"5px", padding: "5px", margin: "5px"}}>
+            <p style={{"fontWeight":"bold"}}>"{message}"</p>
+            <p>From: {memo.name} at {dateString}</p>
+          </div>
+        )
+      }))}
+
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/chenym1992/road-to-web3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          😄 road to web3 😄
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
