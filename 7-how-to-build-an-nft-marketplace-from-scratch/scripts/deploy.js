ab4d5bb4c9c50dc081da110524b54dbf9cb79922
chenym1992 <18251821296@163.com>
2022-11-01 18:33:45 +0800

    chore: update NFTMarketplace.sol

--- 7-how-to-build-an-nft-marketplace-from-scratch/scripts/deploy.js
+++ 7-how-to-build-an-nft-marketplace-from-scratch/scripts/deploy.js
@@ -7,19 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftmarketplace = await NFTMarketplace.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await nftmarketplace.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `NFTMarketplace deployed to ${NFTMarketplace.address}`
   );
 }
 
